4c7b2e2f45b3cba1410e8c238a601341eedab393
Sam Marder <smardercollege@gmail.com>
2026-03-06 14:46:53 +0000

    Linking s3 and lambda via event notification

--- main.tf
+++ main.tf
@@ -67,6 +67,28 @@ resource "aws_lambda_function" "test_lambda" {
   runtime = "python3.12"
 }
 
+resource "aws_lambda_permission" "allow_s3" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.test_lambda.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.noaa_bucket.arn
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.noaa_bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.test_lambda.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "new_archive/"
+    filter_suffix       = ".zip"
+  }
+
+  # This ensures the permission is created BEFORE S3 tries to link to it
+  depends_on = [aws_lambda_permission.allow_s3]
+}
+
 resource "aws_iam_role_policy" "lambda_s3_logs_policy" {
   name = "preprocessor_permissions"
   role = aws_iam_role.iam_for_lambda_noaa_east_2.id
