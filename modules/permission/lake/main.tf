bc1ca23e52817db463b0c2379f8d66f20fe4727a
Sam Marder <smardercollege@gmail.com>
2026-03-25 15:10:55 +0000

    Pushing current latest

--- modules/permission/lake/main.tf
+++ modules/permission/lake/main.tf
@@ -6,12 +6,12 @@ resource "aws_lakeformation_data_lake_settings" "admin" {
   ]
 
   create_database_default_permissions {
-    permissions = []
+    permissions = ["ALL"]
     principal   = "IAM_ALLOWED_PRINCIPALS"
   }
 
   create_table_default_permissions {
-    permissions = []
+    permissions = ["ALL"]
     principal   = "IAM_ALLOWED_PRINCIPALS"
   }
 }
